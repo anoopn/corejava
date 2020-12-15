6d82bf39140f318074be94208ecb075d02b7ccb9
anoopn <anoopn.03@gmail.com>
2020-12-15 23:13:28 +0300

    Basic Datatypes in java

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+class DataTypes{
+	public static void main(String args[]){
+		short sh_data=3;
+		float fl_data=3.3f;
+		double dbl_data=3.6;
+		char ch_data='A';
+		boolean bl_data = true;
+		System.out.println(sh_data);
+		System.out.println(fl_data);
+		System.out.println(dbl_data);
+		System.out.println(ch_data);
+		System.out.println(bl_data);
+	}
+}
